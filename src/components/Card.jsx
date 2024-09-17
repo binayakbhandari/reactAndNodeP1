a71ed5cd78d119b53ffa41eecd408167afb606f5
binayakbhandari <binayakbhandari60@gmail.com>
2024-09-18 00:43:50 +0545

    completed profile card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,9 +7,9 @@ function Card({ person }) {
                     <div className="flex justify-end px-4 pt-4">
                         <button id="dropdownButton" data-dropdown-toggle="dropdown" className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
                             <span className="sr-only">Open dropdown</span>
-                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+                            {/* <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
                                 <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
-                            </svg>
+                            </svg> */}
                         </button>
                     </div>
                     <div className="flex flex-col items-center pb-10 px-4">
