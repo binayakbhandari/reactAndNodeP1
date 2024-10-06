8ff59431be44077d0c735d46ff78d84970eeb7ae
binayakbhandari <binayakbhandari60@gmail.com>
2024-10-06 05:58:47 +0545

    edit card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -20,10 +20,7 @@ function Card({ person }) {
                                 {person.personDescription}
                             </p>
                         </div> */}
-                    <div className="flex mt-6 md:mt-6">
-                        <Link to={person.personLink} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</Link>
-                        <Link to={`/person/${person.id}`} className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Learn More</Link>
-                    </div>
+                    
                 </div>
             </div>
 
