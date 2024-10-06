dd7279bcd7e477b6c55357305e39cf2911e94247
binayakbhandari <binayakbhandari60@gmail.com>
2024-10-06 05:59:09 +0545

    edit card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,14 @@ function Card({ person }) {
     return (
         <>
             <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                
+                <div className="flex justify-end px-4 pt-4">
+                    <button id="dropdownButton" data-dropdown-toggle="dropdown" className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
+                        <span className="sr-only">Open dropdown</span>
+                        {/* <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+                                <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                            </svg> */}
+                    </button>
+                </div>
                 <div className="flex flex-col items-center pb-10 px-4">
                     <div class="relative">
                     <div
@@ -20,7 +27,10 @@ function Card({ person }) {
                                 {person.personDescription}
                             </p>
                         </div> */}
-                    
+                    <div className="flex mt-6 md:mt-6">
+                        <Link to={person.personLink} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</Link>
+                        <Link to={`/person/${person.id}`} className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Learn More</Link>
+                    </div>
                 </div>
             </div>
 
